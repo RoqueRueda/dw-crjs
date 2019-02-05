a18ac33f2ff19ac1ca64647af62275b16f39a7ca
Roque Rueda <skaroque@gmail.com>
2019-02-05 05:50:44 -0600

    Conditional rendering for ImageDetail

--- 05-application/flickr/src/components/ImageDetail.js
+++ 05-application/flickr/src/components/ImageDetail.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 const ImageDetail = props => {
+  const { img } = props;
   return (
     <div className="ui card">
       <div className="image">
-        <img alt="alt" />
+        {img === undefined ? 
+          <div className="ui placeholder">
+            <div className="square image"></div>
+          </div> :
+          <img alt="alt" src={img.url_s} />
+        }
       </div>
       <div className="content">
-        <a href="/" className="header">Roque</a>
+        {img === undefined ?
+          <div className="ui placeholder">
+            <div className="header">
+              <div className="very short line"></div>
+              <div className="medium line"></div>
+            </div>
+          </div> :
+          <a href="/" className="header">{img.id}</a>
+        }
         <div class="meta">
-          <span class="date">05/02/2019</span>
+          {img === undefined ? 
+            <div className="ui placeholder">
+              <div className="very short line"></div>
+            </div> :
+            <span class="date">{img.date}</span>
+          }
         </div>
         <div class="description">
-          Image description
+          {img === undefined ? '' : img.title}
         </div>
       </div>
     </div>
