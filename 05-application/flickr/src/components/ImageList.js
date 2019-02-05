ccca97cec8a244c3ccbe01f0f39dff936251a342
Roque Rueda <skaroque@gmail.com>
2019-02-05 07:27:14 -0600

    Add image list props

--- 05-application/flickr/src/components/ImageList.js
+++ 05-application/flickr/src/components/ImageList.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ListItem from './ListItem';
 
-
 const ImageList = props => {
+  const { images } = props;
+  if (images === undefined) {
+    return (
+      <div className="ui relaxed divided selection list">
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </div>
+    );
+  }
+  
+  const rows = images.map(img => {
+    return (
+      <ListItem key={img.id} img={img} />
+    )
+  })
   return (
     <div className="ui relaxed divided selection list">
-      <ListItem />
-      <ListItem />
-      <ListItem />
+      {rows}
     </div>
   );
 }
